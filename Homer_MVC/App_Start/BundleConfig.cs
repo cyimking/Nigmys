428dc5122340bbb7a00bb447ca2e35d14eaa5ea5
mimusys <mimusys@gmail.com>
2015-09-24 11:30:44 -0700

    Fixed Ipad navbar width newline

--- Homer_MVC/App_Start/BundleConfig.cs
+++ Homer_MVC/App_Start/BundleConfig.cs
@@ -21,10 +21,6 @@ namespace Homer_MVC
                       "~/Scripts/homer.js",
                       "~/Scripts/charts.js"));
 
-            //Landing-Page Custom
-            bundles.Add(new ScriptBundle("~/bundles/landing-page/css").Include(
-                "~/Content/landingStyle.css", new CssRewriteUrlTransform()));
-
             // Animate.css
             bundles.Add(new StyleBundle("~/bundles/animate/css").Include(
                       "~/Vendor/animate.css/animate.min.css"));
@@ -193,6 +189,9 @@ namespace Homer_MVC
                       "~/Vendor/jquery-validation/jquery.validate.min.js"));
 
 
+            //Landing-Page Custom
+            bundles.Add(new ScriptBundle("~/bundles/landing-page/css").Include(
+                "~/Content/landingStyle.css", new CssRewriteUrlTransform()));
         }
 
     }
